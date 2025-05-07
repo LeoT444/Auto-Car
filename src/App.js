fa4a97144dbee937677b6ab4e9ee62454e9ca9f0
RHU4N <rhuan3003@gmail.com>
2025-05-07 11:21:05 -0300

    Add espaço da log,tela de cadastro e ajustando algumas coisas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import styles from "./App.css";
 
 import Menu from "./components/menu";
 import Products from "./components/products";
+import Register from './components/register';
 
 export const App = () => {
   return (
-    <div className={styles.container}>
-
-      <Menu></Menu>
-      <Products></Products>
-
-    </div>
+    <Router>
+      <div className={styles.container}>
+        <Routes>
+          <Route path="/" element={<>
+            <Menu />
+            <Products />
+          </>} />
+          <Route path="/cadastro" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 export default App
